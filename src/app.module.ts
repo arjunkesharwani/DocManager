df8ce90f8b41546cf4aa6d491eac52c06c986e7f
arjunkesharwani <arjun.kesharwani246@gmail.com>
2025-04-06 13:23:57 +0530

    DB configuration postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,16 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'test',
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
